206c165aa33925283adc4b8b34d2db63062fe235
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-02-15 13:28:52 -0800

    Lab 3.4 Completed

--- Lucas D Personal Project/Assets/Scripts/PlayerController.cs
+++ Lucas D Personal Project/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
 
+    private bool isOnGround;
+
     private float speed = 10.0f;
-    private float verticalInput;
     private float horizontalInput;
+
+    private int leftBound = -7;
+    private int rightBound = 18;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        isOnGround = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
         
         if(Input.GetKey(KeyCode.RightArrow))
         {
@@ -30,9 +34,27 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.left * -horizontalInput * speed);
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * verticalInput * speed);
+            playerRb.AddForce(Vector3.up, ForceMode.Impulse);
+            isOnGround = false;
+        }
+        if(transform.position.x < leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > rightBound)
+        {
+            transform.position = new Vector3(rightBound, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }

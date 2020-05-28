63cf903a749ed40e7c4a1ba531d8ea8951b769fb
Lucas Diamantidis <47400463+lykos7@users.noreply.github.com>
2020-05-27 19:41:52 -0700

    1.1-1.3 Complete
    1.4 is partially worked on here.

---
+++ Spring Culminating Project - Lucas D/Assets/DEATH BALL ULTRA/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerEmpty;
+    public GameObject player;
+
+    [SerializeField] private float speed;
+    private float forwardInput;
+    private float horizontalInput;
+
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.AddForce(playerEmpty.transform.forward * speed * forwardInput);
+        playerRb.AddForce(playerEmpty.transform.right * speed * horizontalInput);
+        WinCheck();
+    }
+
+    private void WinCheck()
+    {
+        //This will eventually move the player to the next level
+        if (player.transform.position.z > 52)
+        {
+            Debug.Log("You Win!");
+            Application.Quit();
+        }            
+    }
+}
